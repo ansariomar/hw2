29839605ce0233ba88359a741c3821068cbb049d
ansariomar <omar.ansari@cca.edu>
2018-01-24 08:49:57 -0800

    Create Puzzle-7.js

---
+++ Puzzle-7.js
@@ -0,0 +1,24 @@
+function moveRightTwice() {
+right()
+right()
+}
+
+while (getColor() != "red") {
+down()
+}
+moveRightTwice()
+while (getColor() != "red") {
+up()
+}
+moveRightTwice()
+while (getColor() != "red") {
+down()
+}
+moveRightTwice()
+while (getColor() != "red") {
+up()
+}
+moveRightTwice()
+while (getColor() != "red")
+down()
+right()
