e0446d68c077fb255592170c69aa29ea49d79f03
ansariomar <omar.ansari@cca.edu>
2018-01-24 08:29:35 -0800

    Create Puzzle-2.js

---
+++ Puzzle-2.js
@@ -0,0 +1,20 @@
+function rightThreeTimes() {
+right()
+right()
+right()
+}
+
+var steps = 0
+while (steps < 6) {
+down()
+steps = steps + 1
+}
+
+rightThreeTimes()
+up()
+up()
+right()
+right()
+down()
+down()
+down()
