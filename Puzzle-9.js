77cfb1cab21f94202f3bae05c965b3960fef267b
ansariomar <omar.ansari@cca.edu>
2018-01-24 08:53:18 -0800

    Create Puzzle-9.js

---
+++ Puzzle-9.js
@@ -0,0 +1,24 @@
+var steps = 0
+while (steps < 9) {
+down()
+setColor ("blue")
+steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+right()
+setColor ("blue")
+steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+up()
+setColor ("blue")
+steps = steps + 1
+}
+var steps = 0
+while (steps < 9) {
+left()
+setColor ("blue")
+steps = steps + 1
+}
