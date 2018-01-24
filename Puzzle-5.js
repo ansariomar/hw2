770cbed7b7aa858fa73fe6053b584f2ca46c6863
ansariomar <omar.ansari@cca.edu>
2018-01-24 08:41:46 -0800

    Create Puzzle-5.js

---
+++ Puzzle-5.js
@@ -0,0 +1,28 @@
+function moveRightTwice() {
+right()
+right()
+}
+
+moveRightTwice()
+if (getColor() == "blue") {
+down()
+}
+else {
+up()
+}
+moveRightTwice()
+if (getColor() == "blue") {
+down()
+}
+else {
+up()
+}
+moveRightTwice()
+if (getColor() == "blue") {
+down()
+}
+else {
+up()
+}
+moveRightTwice()
+right()
