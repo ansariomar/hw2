4bf341a4d44b034ab7fc25457431e03fc46af18c
ansariomar <omar.ansari@cca.edu>
2018-01-28 23:57:20 -0800

    Create Puzzle-1.js

---
+++ In-Class/Puzzle-1.js
@@ -0,0 +1,25 @@
+
+function downSevenTimes() {
+down()
+down()
+down()
+down()
+down()
+down()
+down()
+}
+
+function rightFiveTimes() {
+right()
+right()
+right()
+right()
+right()
+}
+
+downSevenTimes()
+rightFiveTimes()
+up()
+up()
+right()
+right()
